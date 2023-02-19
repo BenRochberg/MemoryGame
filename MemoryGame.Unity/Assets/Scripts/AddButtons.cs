bcf559bd561e6a1efc46f884d38da02700ec5f99
Ben Rochberg <benrochberg@gmail.com>
2023-02-19 16:03:48 -0500

    AddButtons Scripts Start
    - Starting the game now reveals 8 interactable buttons with correct names.
    - The AddButtons Script spawns the buttons, no buttons are in the scene at start.

--- MemoryGame.Unity/Assets/Scripts/AddButtons.cs
+++ MemoryGame.Unity/Assets/Scripts/AddButtons.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class AddButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Transform puzzleField;
+
+    [SerializeField] private GameObject btn;
+
+    private void Awake()
     {
-        
+        for (int i = 0; i < 8; i++)
+        {
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(puzzleField, false);
+        }
+
+
+
     }
 }
