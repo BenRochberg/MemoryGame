6358c388e31f89c92a106e0fb936c21b235cb498
Ben Rochberg <benrochberg@gmail.com>
2023-05-12 18:38:26 -0400

    Set up for Levels 3 and 4
    - Implemented Levels Panel for the first 4 levels.
    - Set up code for further implementation of newer levels, as well as animations.

--- MemoryGame.Unity/Assets/Scripts/LevelManager.cs
+++ MemoryGame.Unity/Assets/Scripts/LevelManager.cs
@@ -6,55 +6,56 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject levelSelectPanel, level1Holder, level2Holder;
-    public Button level1Btn, level2Btn, backBtn;
+    public GameObject levelSelectPanel, firstLevelsPanel, level1Holder, level2Holder, level3Holder, level4Holder;
+    public Button level1Btn, level2Btn, level3Btn, level4Btn, backBtn;
     public float completeTime;
     public float bounceTime;
     public float delayTime;
     void Start()  //Each GameObject and Button in the Main Menu start off at 0 alpha. On Start, these 4 lines turn up the alpha to 255 (0f = 0 Alpha, 1f = 255 Alpha). There are floats to change how fast the Alpha changes (completeTime) and how long it waits to start the animation (delayTime).
     {
         LeanTween.alpha(levelSelectPanel.GetComponent<RectTransform>(), 1f, completeTime).setDelay(delayTime);
+        LeanTween.alpha(firstLevelsPanel.GetComponent<RectTransform>(), 1f, completeTime).setDelay(delayTime);
         LeanTween.alpha(level1Btn.GetComponent<RectTransform>(), 1f, completeTime).setDelay(delayTime);
         LeanTween.alpha(level2Btn.GetComponent<RectTransform>(), 1f, completeTime).setDelay(delayTime);
+        LeanTween.alpha(level3Btn.GetComponent<RectTransform>(), 1f, completeTime).setDelay(delayTime);
+        LeanTween.alpha(level4Btn.GetComponent<RectTransform>(), 1f, completeTime).setDelay(delayTime);
         LeanTween.alpha(backBtn.GetComponent<RectTransform>(), 1f, completeTime).setDelay(delayTime);
         LeanTween.scale(level1Holder, new Vector2(1f, 1f), bounceTime).setEase(LeanTweenType.easeInQuint);
         LeanTween.scale(level2Holder, new Vector2(1f, 1f), bounceTime).setEase(LeanTweenType.easeInQuint);
+        LeanTween.scale(level3Holder, new Vector2(1f, 1f), bounceTime).setEase(LeanTweenType.easeInQuint);
+        LeanTween.scale(level4Holder, new Vector2(1f, 1f), bounceTime).setEase(LeanTweenType.easeInQuint);
     }
-    public void PlayOneClicked()  //When you click on the Play Button on the Main Menu, it sets both Buttons' interability to false and turns down each GameObject and Buttons Alpha to 0. The setOnComplete function sends the player to the second scene (GameScene) once the animations complete.
+
+    void SetUpForNextLevel()
     {
         level1Btn.interactable = false;
         level2Btn.interactable = false;
         backBtn.interactable = false;
         LeanTween.scale(level1Holder, new Vector2(0f, 0f), bounceTime).setEase(LeanTweenType.easeOutQuint);
         LeanTween.scale(level2Holder, new Vector2(0f, 0f), bounceTime).setEase(LeanTweenType.easeOutQuint);
+        LeanTween.scale(level3Holder, new Vector2(0f, 0f), bounceTime).setEase(LeanTweenType.easeOutQuint);
+        LeanTween.scale(level4Holder, new Vector2(0f, 0f), bounceTime).setEase(LeanTweenType.easeOutQuint);
         LeanTween.alpha(level1Btn.GetComponent<RectTransform>(), 0f, completeTime);
         LeanTween.alpha(level2Btn.GetComponent<RectTransform>(), 0f, completeTime);
+        LeanTween.alpha(level3Btn.GetComponent<RectTransform>(), 0f, completeTime);
+        LeanTween.alpha(level4Btn.GetComponent<RectTransform>(), 0f, completeTime);
         LeanTween.alpha(backBtn.GetComponent<RectTransform>(), 0f, completeTime);
-        LeanTween.alpha(levelSelectPanel.GetComponent<RectTransform>(), 0f, completeTime).setOnComplete(GoToOne);
+        LeanTween.alpha(firstLevelsPanel.GetComponent<RectTransform>(), 0f, completeTime);
+    }
+    public void PlayOneClicked()  //When you click on the Play Button on the Main Menu, it sets both Buttons' interability to false and turns down each GameObject and Buttons Alpha to 0. The setOnComplete function sends the player to the second scene (GameScene) once the animations complete.
+    {
+        SetUpForNextLevel();
+        LeanTween.alpha(levelSelectPanel.GetComponent<RectTransform>(), 0f, completeTime).setDelay(0.5f).setOnComplete(GoToOne);
     }
 
     public void PlayTwoClicked()  //When you click on the Play Button on the Main Menu, it sets both Buttons' interability to false and turns down each GameObject and Buttons Alpha to 0. The setOnComplete function sends the player to the second scene (GameScene) once the animations complete.
     {
-        level1Btn.interactable = false;
-        level2Btn.interactable = false;
-        backBtn.interactable = false;
-        LeanTween.scale(level1Holder, new Vector2(0f, 0f), bounceTime).setEase(LeanTweenType.easeOutQuint);
-        LeanTween.scale(level2Holder, new Vector2(0f, 0f), bounceTime).setEase(LeanTweenType.easeOutQuint);
-        LeanTween.alpha(level1Btn.GetComponent<RectTransform>(), 0f, completeTime);
-        LeanTween.alpha(level2Btn.GetComponent<RectTransform>(), 0f, completeTime);
-        LeanTween.alpha(backBtn.GetComponent<RectTransform>(), 0f, completeTime);
+        SetUpForNextLevel();
         LeanTween.alpha(levelSelectPanel.GetComponent<RectTransform>(), 0f, completeTime).setOnComplete(GoToTwo);
     }
     public void Back()
     {
-        level1Btn.interactable = false;
-        level2Btn.interactable = false;
-        backBtn.interactable = false;
-        LeanTween.scale(level1Holder, new Vector2(0f, 0f), bounceTime).setEase(LeanTweenType.easeOutQuint);
-        LeanTween.scale(level2Holder, new Vector2(0f, 0f), bounceTime).setEase(LeanTweenType.easeOutQuint);
-        LeanTween.alpha(level1Btn.GetComponent<RectTransform>(), 0f, completeTime);
-        LeanTween.alpha(level2Btn.GetComponent<RectTransform>(), 0f, completeTime);
-        LeanTween.alpha(backBtn.GetComponent<RectTransform>(), 0f, completeTime);
+        SetUpForNextLevel();
         LeanTween.alpha(levelSelectPanel.GetComponent<RectTransform>(), 0f, completeTime).setOnComplete(GoToMenu);
     }
     void GoToOne() => SceneManager.LoadScene("GameScene1");
